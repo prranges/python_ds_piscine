c9352b72e20d656682fc1e5770e3e0d338d37f70
Permann Rranges <prranges@at-o9.msk.21-school.ru>
2022-09-08 15:50:03 +0300

    day_00 ex01 done

--- Day_00/ex00/hh.sh
+++ Day_00/ex00/hh.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 1 ]
 then
-    curl -k -H 'User-Agent: api-test-agent' 'https://api.hh.ru/vacancies?text='"${1// /%20}"\&per_page\=20\&search_field\=name''  | jq '.' > hh.json
+    curl -k -H 'User-Agent: api-test-agent' 'https://api.hh.ru/vacancies?text='"${1// /%20}"'' | jq > hh.json
 else
     echo "Enter: ./hh.sh 'data scientist'"
 fi
