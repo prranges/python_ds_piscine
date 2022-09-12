2b88efadf671e518126437291a15546bb58cfec6
Permann Rranges <prranges@at-o9.msk.21-school.ru>
2022-09-12 20:54:23 +0300

    Day_01 ex08 done

--- Day_01/ex08/letter_starter.py
+++ Day_01/ex08/letter_starter.py
@@ -1,6 +1,15 @@
-def f():
-    pass
+import sys
+
+def start_email(email):
+    with open('employees.tsv', 'r') as input:
+        file = input.readlines()
+        for line in file[1:]:
+            name = line.split('\t')[0].strip()
+            mail = line.split('\t')[2].strip()
+            if mail == email:
+                print(f'Dear {name}, welcome to our team. We are sure that it will be a pleasure to work with you. That’s a precondition for the professionals that our company hires.')
 
 
 if __name__ == '__main__':
-    f()
+    if len(sys.argv) == 2:
+        start_email(sys.argv[1])
--- Day_01/ex08/names_extractor.py
+++ Day_01/ex08/names_extractor.py
@@ -1,6 +1,15 @@
-def f():
-    pass
+import sys
+
+def extract_name(file):
+    with open(file, 'r') as input:
+        with open('employees.tsv', 'w') as output:
+            output.write('Name\tSurname\tE-mail\n')
+            for line in input:
+                name = line.split('.')[0].strip().capitalize()
+                surname = line.split('.')[1].strip().split('@')[0].capitalize()
+                output.write(f'{name}\t{surname}\t{line}')
 
 
 if __name__ == '__main__':
-    f()
+    if len(sys.argv) == 2:
+        extract_name(sys.argv[1])
