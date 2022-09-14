37ca497c64d4dae99f4478ffcfc1a3533b954d5b
Permann Rranges <prranges@at-r6.msk.21-school.ru>
2022-09-14 17:23:22 +0300

    Day_03 ex00

---
+++ Day_03/ex00/venv.py
@@ -0,0 +1,6 @@
+#!/Users/prranges/projects/Python_DS_Piscine/Day_03/ex00/prranges/bin/python3
+import os
+
+
+if __name__ == '__main__':
+    print(f"Your current virtual env is {os.environ['VIRTUAL_ENV']}")
