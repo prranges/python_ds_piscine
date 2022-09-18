7f16f816c7252d4c7a17f253434a5267abcd1c8a
Дмитрий Краснощёков <d1ma5@Air-Dmitrij.Dlink>
2022-09-18 16:03:00 +0300

    Day_04 ex05

---
+++ Day_04/ex05/generator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+import psutil
+
+def read_lines(file):
+    with open(file, 'r') as input:
+        for line in file:
+            yield line
+
+def main(file):
+    try:
+        lines = read_lines(file)
+        for line in lines:
+            pass
+        process = psutil.Process()
+        mem = process.memory_info().rss / (2 ** 30)
+        cpu = process.cpu_times()
+        print(f'Peak Memory Usage = {mem:.3f} Gb')
+        print(f'User Time + System Time = {cpu.user + cpu.system:.2f}s')
+    except:
+        raise Exception("Error.")
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        raise Exception('Argument error. Usage: ./generator.py  <path to csv file>')
